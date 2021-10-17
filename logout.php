b9b70c6d2e138ca5a4308fff1d5e9c7bce2f31bc
easy web script <92661782+easywebscript@users.noreply.github.com>
2021-10-17 15:46:39 +0600

    Update logout.php

--- logout.php
+++ logout.php
@@ -5,7 +5,7 @@ session_start();
 // destroy all session and go to login page.
 session_destroy();
 session_start();
-$_SESSION['error_msg'] = "Your are logout. Please Login again.";
+$_SESSION['error_msg'] = "Your are logged out. Please Login again.";
 header('Location: index.php');
 
 exit;
